2d7a139dbaf2213a4286611a7da01a7971b238eb
David-Peralta-Rd <crdavidperalta@gmail.com>
2025-10-17 21:18:43 -0500

    Modificando(lua/plugins/example.lua): plugin desactivado: Desactivando telescope de lua/plugins/example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -28,7 +28,7 @@ return {
   },
 
   -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  { "folke/trouble.nvim",      enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
   {
@@ -40,28 +40,28 @@ return {
     end,
   },
 
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
+  --  -- change some telescope options and a keymap to browse plugin files
+  --  {
+  --    "nvim-telescope/telescope.nvim",
+  --    keys = {
+  --      -- add a keymap to browse plugin files
+  --      -- stylua: ignore
+  --      {
+  --        "<leader>fp",
+  --        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+  --        desc = "Find Plugin File",
+  --      },
+  --    },
+  --    -- change some options
+  --    opts = {
+  --      defaults = {
+  --        layout_strategy = "horizontal",
+  --        layout_config = { prompt_position = "top" },
+  --        sorting_strategy = "ascending",
+  --        winblend = 0,
+  --      },
+  --    },
+  --  },
 
   -- add pyright to lspconfig
   {
@@ -84,7 +84,7 @@ return {
       init = function()
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
           -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
